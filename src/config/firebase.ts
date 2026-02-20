a2453900b0cc62086260bfee767b81b8f48bb8a6
elcalebitsur <s22120212@alumnos.itsur.edu.mx>
2026-02-19 23:07:35 -0600

    security: Move Firebase keys to .env and add to gitignore

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDy2MM71NejRmX59FbeLjdUCKjIkiws09c",
-    authDomain: "marketplace-escolar.firebaseapp.com",
-    projectId: "marketplace-escolar",
-    storageBucket: "marketplace-escolar.firebasestorage.app",
-    messagingSenderId: "348495555820",
-    appId: "1:348495555820:web:6338d218abc5999e0209c9",
-    measurementId: "G-JLZ43F16SL"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
